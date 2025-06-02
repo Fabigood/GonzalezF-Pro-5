ce8a902e13b0ef7f68e3c50fafb69f8afcf5b132
Fabio Gonzalez <fabio.gonzalez.fiallos@udla.edu.ec>
2025-06-02 07:42:09 -0500

    Add project files.

---
+++ GonzalezFExP2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GonzalezFExP2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GonzalezFExP2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GonzalezFExP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
