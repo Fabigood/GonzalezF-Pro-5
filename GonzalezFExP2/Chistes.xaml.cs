893bd0effd373ab8efea7495258656191d928659
Fabio Gonzalez <fabio.gonzalez.fiallos@udla.edu.ec>
2025-06-02 08:16:25 -0500

    creamos la page de chistes se creo el xaml y tambien la parte de cs de xaml

--- GonzalezFExP2/Chistes.xaml.cs
+++ GonzalezFExP2/Chistes.xaml.cs
@@ -1,9 +1,31 @@
+using System.Net.Http.Json;
+
 namespace GonzalezFExP2;
 
 public partial class Chistes : ContentPage
 {
-	public Chistes()
-	{
-		InitializeComponent();
-	}
+    private readonly HttpClient _httpClient;
+
+    public Chistes()
+    {
+        InitializeComponent();
+        _httpClient = new HttpClient();
+        CargarChiste();
+    }
+
+    private async void CargarChiste()
+    {
+    }
+
+    private void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        CargarChiste();
+    }
+}
+
+
+public class Joke
+{
+    public string Setup { get; set; }
+    public string Punchline { get; set; }
 }
